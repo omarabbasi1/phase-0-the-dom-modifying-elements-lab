bc34ec917189bc01dbcc6eec27db7bb8ec16e9a5
omarabbasi <omarkabbasi@outlook.com>
2022-02-04 15:12:39 -0800

    completed 2.0

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 // Write your code here!
-main.remove();
+// const element = document.createElement("div");
 
-const newHeader = "h1";
+// document.body.append(element);
 
-const newHeader = document.createElement('h1');
+// const ul = document.createElement("ul");
 
-newHeader.setAttribute("id", "myHeader");
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+//   li.textContent = "Hi there!";
+
+
+// element.append(ul);
+
+// const ul = document.getElementsByTagName("ul")[0];
+// const secondChild = ul.querySelector("li:nth-child(2)");
+// ul.removeChild(secondChild);
+
+// ul.remove();
+// }
+// const main = document.getElementById("main");
+// main.innerHTML =
+//   "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+  const testVar = "hi"
+
+  /* Enter the code to remove the node element here */
+  document.querySelector("main#main").remove();
+  
+  
+  /* Create your new element here and assign it to newHeader */
+  const newHeader = document.createElement("h1");
+  newHeader.id = "victory"
+  newHeader.innerHTML = "Flatiron School is the champion!";
+  newHeader.className = "victory";
